5f203bb534d95740e6fa7ff8065d37ef99c25ba8
EmLiu0314 <emilyliu52106@gmail.com>
2024-03-19 22:52:27 -0500

    Foregin Money Exchange

---
+++ ForeignCurrencyExchange/SelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectionView.swift
+//  ForeignCurrencyExchange
+//
+//  Created by Liu, Emily on 3/19/24.
+//
+
+import SwiftUI
+
+struct SelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectionView()
+}
