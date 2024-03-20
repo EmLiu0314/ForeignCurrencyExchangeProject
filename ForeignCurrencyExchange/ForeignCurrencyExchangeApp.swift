d449c50c65c118692514d1f1e555ad6ccf6d7f7b
EmLiu0314 <emilyliu52106@gmail.com>
2024-03-19 22:43:23 -0500

    Initial Commit

---
+++ ForeignCurrencyExchange/ForeignCurrencyExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForeignCurrencyExchangeApp.swift
+//  ForeignCurrencyExchange
+//
+//  Created by Liu, Emily on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ForeignCurrencyExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
